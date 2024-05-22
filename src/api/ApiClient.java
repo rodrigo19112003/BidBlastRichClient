7a4127183a09b44aa2cdf2ec6bad71f3b86aaaf1
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-05-22 04:32:01 -0600

    feat(ApiClient): implements the connection to the api

---
+++ src/api/ApiClient.java
@@ -0,0 +1,30 @@
+
+package api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+
+public class ApiClient {
+    private static final ApiClient apiClient = new ApiClient();
+    public static final String API_BASE_URL = "http://10.0.2.2:3000/api/";
+    private final Retrofit retrofit;
+    private IRegisterService registerService;
+
+    public static ApiClient getInstance() {
+        return apiClient;
+    }
+
+    private ApiClient() {
+        retrofit = new Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build();
+    }
+    public IRegisterService getRegisterService() {
+        if (registerService == null) {
+            registerService = retrofit.create(IRegisterService.class);
+        }
+        return registerService;
+    }
+}
