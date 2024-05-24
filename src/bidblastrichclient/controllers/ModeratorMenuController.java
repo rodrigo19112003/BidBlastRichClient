35f6cb9f981a615dfc31f414f9c35e1a2564a5f1
Angel Garcia <adjcg15@gmail.com>
2024-05-23 22:03:19 -0600

    feat(moderator menu): creates general structure for moderator menu gui

--- src/bidblastrichclient/controllers/ModeratorMenuController.java
+++ src/bidblastrichclient/controllers/ModeratorMenuController.java
@@ -2,6 +2,8 @@ package bidblastrichclient.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class ModeratorMenuController implements Initializable {
@@ -10,4 +12,12 @@ public class ModeratorMenuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
     }    
+
+    @FXML
+    private void btnAssessAuctionClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnAuctionCategoriesListClick(ActionEvent event) {
+    }
 }
