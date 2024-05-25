f3a055628eec29219e98f4a85773a58a28b85847
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-25 13:52:01 -0600

    feat(modify auction category): implements connection to api to modify auction category

--- src/bidblastrichclient/controllers/AuctionCategoryFormController.java
+++ src/bidblastrichclient/controllers/AuctionCategoryFormController.java
@@ -1,16 +1,23 @@
 package bidblastrichclient.controllers;
 
+import api.requests.auctioncategories.AuctionCategoryBody;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import lib.ValidationToolkit;
 import model.AuctionCategory;
+import repositories.AuctionCategoriesRepository;
+import repositories.IEmptyProcessStatusListener;
+import repositories.ProcessErrorCodes;
 
 public class AuctionCategoryFormController implements Initializable {
 
@@ -26,10 +33,11 @@ public class AuctionCategoryFormController implements Initializable {
     private Label lblDescriptionError;
     @FXML
     private Label lblKeywordsError;
+    private int idAuctionCategory;
     private String title;
     private String description;
     private String keywords;
-    private Boolean isEdition;
+    private boolean isEdition;
     @FXML
     private Button btnSaveTitle;
     @FXML
@@ -40,6 +48,7 @@ public class AuctionCategoryFormController implements Initializable {
     }    
     
     public void setAuctionCategoryInformation(AuctionCategory category, boolean isEdition) {
+        this.idAuctionCategory = category.getId();
         this.title = category.getTitle();
         this.description = category.getDescription();
         this.keywords = category.getKeywords();
@@ -66,6 +75,30 @@ public class AuctionCategoryFormController implements Initializable {
         lblDescriptionError.setVisible(false);
         lblKeywordsError.setVisible(false);
     }
+    
+    private boolean verifyTextFields() {
+        boolean areValid = 
+                !tfTitle.getText().isEmpty() &&
+                !tfDescription.getText().isEmpty() &&
+                !tfKeywords.getText().isEmpty() &&
+                ValidationToolkit.areValidKeywords(tfKeywords.getText());
+        
+        System.out.println(areValid);
+        
+        return areValid;
+    }
+    
+    private void showTextFieldsErrorsMessage() {
+        if (tfTitle.getText().isEmpty()) {
+            lblTitleError.setVisible(true);
+        }
+        if (tfDescription.getText().isEmpty()) {
+            lblDescriptionError.setVisible(true);
+        }
+        if (tfKeywords.getText().isEmpty() || !ValidationToolkit.areValidKeywords(tfKeywords.getText())) {
+            lblKeywordsError.setVisible(true);
+        }
+    }
 
     @FXML
     private void btnGoToAuctionCategoriesListClick(MouseEvent event) {
@@ -74,10 +107,67 @@ public class AuctionCategoryFormController implements Initializable {
 
     @FXML
     private void btnCancelModifyAuctionCategoryClick(ActionEvent event) {
+        // TODO Redirección a la lista de categorías
     }
 
     @FXML
     private void btnSaveAuctionCategoryClick(ActionEvent event) {
+        hideErrorMessages();
+        isEdition = true;
+        if (verifyTextFields()) {
+            if (isEdition) {
+                idAuctionCategory = 1;
+                modifyAuctionCategory();
+            } else {
+                registerAuctionCategory();
+            }
+        } else {
+            showTextFieldsErrorsMessage();
+        }
     }
     
+    private void registerAuctionCategory() {
+        // TODO
+    }
+    
+    private void modifyAuctionCategory() {
+        new AuctionCategoriesRepository().updateAuctionCategory(
+           idAuctionCategory,
+            new AuctionCategoryBody(
+                    tfTitle.getText(),
+                    tfDescription.getText(),
+                    tfKeywords.getText()
+            ),
+            new IEmptyProcessStatusListener() {
+
+                @Override
+                public void onSuccess() {
+                    Platform.runLater(() -> {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("Categoría modificada exitosamente");
+                        alert.setHeaderText(null);
+                        alert.setContentText("Se actualizó la información de la "
+                                + "categoría de forma correcta");
+                        alert.showAndWait();
+                    });
+                }
+
+                @Override
+                public void onError(ProcessErrorCodes errorCode) {
+                    Platform.runLater(() -> {
+                        Alert alert = new Alert(Alert.AlertType.WARNING);
+                        alert.setTitle("Error de conexión");
+                        alert.setHeaderText(null);
+                        alert.setContentText("Ocurrió un error al modificar la"
+                                + " categoría, inténtelo más tarde");
+                        alert.showAndWait();
+                    });
+                }
+            }
+        );
+    }
+    
+    private void goToAuctionCategoriesList() {
+        // TODO
+    }
 }
